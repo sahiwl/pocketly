76b298c91fe2cc939613abc8ff33ce331e86a6fc
sahiwl <piyushkrray1@gmail.com>
2025-11-09 23:01:06 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='flex justify-center  text-amber-600'>in works</p>
+    </>
+  )
+}
+
+export default App
